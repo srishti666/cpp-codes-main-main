1773862e6adf592727a8f491532713fe5d1742b7
srishti <srishtimanna6@gmail.com>
2025-04-21 08:00:19 +0530

    .

--- dsa/Binary Search/BS on answers/5_find the smallest divisor given a threshold.cpp
+++ dsa/Binary Search/BS on answers/5_find the smallest divisor given a threshold.cpp
@@ -1,7 +1,7 @@
 //question: 
 
 //brute force -- TLE 
-
+//TC: O(max(nums) * n)
 class Solution {
     public:
         int smallestDivisor(vector<int>& nums, int threshold) {    
@@ -20,3 +20,31 @@ class Solution {
         return maxNum; 
     }
 }
+
+
+//optimized
+//TC: O(log(max) * n)
+
+class Solution {
+    public:
+        int sumOfDivisions(vector<int>& nums, int divisor) {
+            int sum = 0;
+            for(int i=0; i<nums.size(); i++){
+                sum = sum + ceil((double(nums[i])/double(divisor)));
+            }
+            return sum;
+        }
+        int smallestDivisor(vector<int>& nums, int threshold) {
+            int low = 1, high = *max_element(nums.begin(), nums.end());
+            int ans = 0;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(sumOfDivisions(nums, mid) <= threshold){
+                    ans = mid;
+                    high = mid - 1;
+                }
+                else low = mid + 1;
+            }
+            return ans;
+        }
+    };
