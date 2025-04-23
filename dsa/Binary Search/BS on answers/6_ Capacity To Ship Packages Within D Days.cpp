9ce3b6b1661e51b8a1049fe5ce9053fa8c50040c
srishti <srishtimanna6@gmail.com>
2025-04-23 16:21:26 +0530

    optimized

--- dsa/Binary Search/BS on answers/6_ Capacity To Ship Packages Within D Days.cpp
+++ dsa/Binary Search/BS on answers/6_ Capacity To Ship Packages Within D Days.cpp
@@ -35,5 +35,36 @@ class Solution {
 
 
     //optimized solution using binary search
-
-    
+    //RC:  O(N * log(sum(weights[]) - max(weights[]) + 1))
+    class Solution {
+        public:
+        
+            int findDays(vector<int> & weights, int cap){
+                int days = 1;
+                int load = 0;
+                int n = weights.size();
+                for(int i = 0; i< n; i++){
+                    if(load + weights[i] > cap){
+                        days++;
+                        load = weights[i];
+                    }
+                    else load += weights[i];
+                }
+                return days;
+            }
+            int shipWithinDays(vector<int>& weights, int days) {
+                int maxi = *max_element(weights.begin(), weights.end());
+                int sum = accumulate(weights.begin(), weights.end(), 0);
+        
+                int low = maxi;
+                int high = sum;
+                while(low <= high){
+                    int mid = (low + high)/2;
+                    if(findDays(weights, mid) <= days){
+                        high = mid - 1;
+                    }
+                    else low = mid + 1;
+                }
+                return low;
+            }
+        };
