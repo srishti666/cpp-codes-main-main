36e6e328be4fb0169e553b110a4644e59f5e97c7
srishti <srishtimanna6@gmail.com>
2025-04-24 11:08:03 +0530

    optimal

--- dsa/Binary Search/BS on answers/7_find missing positive number.cpp
+++ dsa/Binary Search/BS on answers/7_find missing positive number.cpp
@@ -13,3 +13,24 @@ class Solution {
             return k;
         }
     };
+
+    //optimized : binary search
+    //TC: O(logn)
+
+    class Solution {
+        public:
+        
+            int findKthPositive(vector<int>& arr, int k) {
+                int low = 0; int high = arr.size()-1;
+        
+                while(low <= high){
+                    int mid = (low + high)/2;
+                    int missing = arr[mid] - (mid + 1); 
+                    if(missing < k){
+                        low = mid + 1;
+                    }
+                    else high = mid - 1;
+                }
+                return (k+high+1);
+            }
+        };
