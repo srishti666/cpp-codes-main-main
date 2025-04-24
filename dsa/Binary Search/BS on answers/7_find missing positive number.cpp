6650af4e25393dcd24bb2846305ddfd8774feba8
srishti <srishtimanna6@gmail.com>
2025-04-24 08:38:58 +0530

    brute force

---
+++ dsa/Binary Search/BS on answers/7_find missing positive number.cpp
@@ -0,0 +1,15 @@
+//question: 
+
+
+//brute force TC: O(n)
+class Solution {
+    public:
+    
+        int findKthPositive(vector<int>& arr, int k) {
+            for(int i=0; i<arr.size(); i++){
+                if(arr[i] <= k) k++;
+                else break;
+            }
+            return k;
+        }
+    };
