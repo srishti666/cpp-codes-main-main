f2f0f59e5d74de5f89bf3269c112b75cbad72b1f
srishti <srishtimanna6@gmail.com>
2025-04-25 16:38:08 +0530

    brute

--- dsa/Binary Search/BS on 2D arrays/find the row with max number of 1s.cpp
+++ dsa/Binary Search/BS on 2D arrays/find the row with max number of 1s.cpp
@@ -1,21 +1,31 @@
 //question: https://leetcode.com/problems/row-with-maximum-ones/description/
 
-
-int rowWithMax1s(vector<vector<int>> &matrix, int n, int m) {
-    int cnt_max = 0;
-    int index = -1;
-
-    //traverse the matrix:
-    for (int i = 0; i < n; i++) {
-        int cnt_ones = 0;
-        for (int j = 0; j < m; j++) {
-            cnt_ones += matrix[i][j];
+class Solution {
+    public:
+        vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+            int n = mat.size(); // Number of rows
+            int m = mat[0].size(); // Number of cols
+    
+            int cnt_max = 0;
+            int index = 0;
+    
+            for(int i=0; i<n; i++){
+                int cnt_ones = 0;
+                for(int j = 0; j<m; j++){
+                    cnt_ones += mat[i][j];
+                }
+                    if(cnt_ones > cnt_max){
+                        cnt_max = cnt_ones;
+                        index = i;
+                    }
+                }
+                return {index,cnt_max} ;
         }
-        if (cnt_ones > cnt_max) {
-            cnt_max = cnt_ones;
-            index = i;
-        }
-    }
-    return index;
-}
-
+    };
+/*
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 1, 1, 1]
+    
+    */
