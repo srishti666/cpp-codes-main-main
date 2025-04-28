adc2162bb54474e91534aa62d6fa93406962d260
srishti <srishtimanna6@gmail.com>
2025-04-28 21:53:08 +0530

    optimized

--- dsa/Binary Search/BS on 2D arrays/find the row with max number of 1s.cpp
+++ dsa/Binary Search/BS on 2D arrays/find the row with max number of 1s.cpp
@@ -36,3 +36,39 @@ class Solution {
 
     //optimized
 
+
+class Solution {
+public:
+int lowerBound(vector<int> &arr, int x){
+    int n=arr.size();
+    int low=0;
+    int high = n-1;
+    sort(arr.begin(),arr.end());
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid] >= x){
+            ans=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int maxCount=0;
+        int index=0;
+        vector<int> ans;
+        for(int i=0; i<m; i++){
+            int onesCount = n-lowerBound(mat[i], 1);
+            if(onesCount > maxCount){
+                maxCount = onesCount;
+                index=i;
+            }
+        }
+        return {index, maxCount};
+    }
+};
