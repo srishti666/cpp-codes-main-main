ca0818807a92b380a7a3a3b16269a67fc1a70c79
srishti <srishtimanna6@gmail.com>
2025-05-02 11:49:53 +0530

    reverse words in a string

---
+++ dsa/Strings/Basic Strings/2_reverse words in a string.cpp
@@ -0,0 +1,36 @@
+//question: https://leetcode.com/problems/reverse-words-in-a-string/
+
+//brute force using stack 
+
+class Solution {
+    public:
+        string reverseWords(string s) {
+            s += " ";  
+            stack<string> st;
+            string str = "";
+    
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == ' ') {
+                    if (!str.empty()) {  
+                        st.push(str);
+                        str = "";
+                    }
+                } else {
+                    str += s[i];
+                }
+            }
+    
+            string ans = "";
+    
+            // Pop words from the stack to reverse their order
+            while (!st.empty()) {
+                ans += st.top();
+                st.pop();
+                if (!st.empty()) {
+                    ans += " ";  
+                }
+            }
+    
+            return ans;
+        }
+    };
