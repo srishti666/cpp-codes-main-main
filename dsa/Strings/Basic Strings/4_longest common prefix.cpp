cba9da98a7830f1a9c23fcaca084704b68373243
srishti <srishtimanna6@gmail.com>
2025-05-05 21:20:36 +0530

    another solution

--- dsa/Strings/Basic Strings/4_longest common prefix.cpp
+++ dsa/Strings/Basic Strings/4_longest common prefix.cpp
@@ -16,19 +16,18 @@ class Solution {
 }
 
 
+    //another solution TC: O(m*n)
 
-//another
-class Solution {
-    public:
-        string longestCommonPrefix(vector<string>& strs) {
-            sort(strs.begin(),strs.end());
-            int a=strs.size();
-            string n=strs[0],m=strs[a-1],ans="";
-            for(int i=0;i<n.size();i++){
-                if(n[i]==m[i]){ans+=n[i];}
-                else break;
+    class Solution {
+        public:
+            string longestCommonPrefix(vector<string>& strs) {
+                string ans = "";
+                sort(strs.begin(), strs.end());
+                int i=0;
+                while(i<strs[0].length() and strs[0][i] == strs[strs.size()-1][i]){
+                    ans += strs[0][i];
+                    i++;
+                }
+                return ans;
             }
-            return ans;
-            
-        }
-    };
+        };
