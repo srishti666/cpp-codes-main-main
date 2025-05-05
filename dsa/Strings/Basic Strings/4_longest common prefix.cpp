c243f06b9e51bf050827a342a9b724c2fea1001c
srishti <srishtimanna6@gmail.com>
2025-05-05 06:59:28 +0530

    brute force

--- dsa/Strings/Basic Strings/4_longest common prefix.cpp
+++ dsa/Strings/Basic Strings/4_longest common prefix.cpp
@@ -1,5 +1,16 @@
 //question: https://leetcode.com/problems/longest-common-prefix/
 
 
-// optimized
+// brute force
 
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int index=1;index<strs.length;index++){
+            while(strs[index].indexOf(prefix) != 0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
