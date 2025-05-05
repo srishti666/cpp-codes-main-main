d6d42a939b4e9e7f3efa2b79ef08cf5df037eec9
srishti <srishtimanna6@gmail.com>
2025-05-05 19:58:33 +0530

    tc

--- dsa/Strings/Basic Strings/4_longest common prefix.cpp
+++ dsa/Strings/Basic Strings/4_longest common prefix.cpp
@@ -2,7 +2,7 @@
 
 
 // brute force
-
+// TC: O(m * n)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
@@ -14,3 +14,21 @@ class Solution {
         return prefix;
     }
 }
+
+
+
+//another
+class Solution {
+    public:
+        string longestCommonPrefix(vector<string>& strs) {
+            sort(strs.begin(),strs.end());
+            int a=strs.size();
+            string n=strs[0],m=strs[a-1],ans="";
+            for(int i=0;i<n.size();i++){
+                if(n[i]==m[i]){ans+=n[i];}
+                else break;
+            }
+            return ans;
+            
+        }
+    };
