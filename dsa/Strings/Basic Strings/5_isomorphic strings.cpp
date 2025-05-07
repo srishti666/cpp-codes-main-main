93119d5e394dd02aaf860e4c36e7d7b9e94ab5cf
srishti <srishtimanna6@gmail.com>
2025-05-07 19:58:54 +0530

    brute force

--- dsa/Strings/Basic Strings/5_isomorphic strings.cpp
+++ dsa/Strings/Basic Strings/5_isomorphic strings.cpp
@@ -1,1 +1,27 @@
 //question: https://leetcode.com/problems/longest-common-prefix/description/
+
+
+class Solution {
+    public:
+        bool isIsomorphic(string s, string t) {
+            if(s.length() != t.length()) return false;
+    
+            int size=s.length();
+            for(int i=0;i<size;i++){
+                char first=s[i];
+                char second=t[i];
+    
+                for(int j=i;j<size;j++){
+                    if(s[j]==first && t[j]!=second){
+                        return false;
+                    }
+                     if (t[j] == second && s[j] != first)
+                {
+                    return false;
+                }
+                }
+    
+            }
+            return true;
+        }
+    };
