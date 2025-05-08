e668b6dda628a9d5201b153c140cc370466513b4
srishti <srishtimanna6@gmail.com>
2025-05-08 22:30:23 +0530

    optimized

--- dsa/Strings/Basic Strings/5_isomorphic strings.cpp
+++ dsa/Strings/Basic Strings/5_isomorphic strings.cpp
@@ -25,3 +25,30 @@ class Solution {
             return true;
         }
     };
+
+
+    //optimized
+    class Solution {
+        public:
+            bool isIsomorphic(string s, string t) {
+                if (s.length() != t.length()) return false;
+                
+                int mapST[256], mapTS[256];
+                fill(begin(mapST), end(mapST), -1);
+                fill(begin(mapTS), end(mapTS), -1);
+                
+                for (int i = 0; i < s.length(); i++) {
+                    char chS = s[i], chT = t[i];
+                    
+                    if (mapST[chS] == -1 && mapTS[chT] == -1) {
+                        mapST[chS] = chT;
+                        mapTS[chT] = chS;
+                    } else {
+                        if (mapST[chS] != chT || mapTS[chT] != chS) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        };
